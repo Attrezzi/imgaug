801e5a1de6d9af4e96335a68c8bdae94e038ca68
Lithium <gabriele.aldeghi.27@gmail.com>
2020-04-21 00:53:10 +0200

    Updated to reflect mode

--- imgaug/augmenters/size.py
+++ imgaug/augmenters/size.py
@@ -2750,8 +2750,8 @@ class PadToFixedSize(meta.Augmenter):
 
         # set these to None to use the same values as sampled for the
         # images (not tested)
-        self._pad_mode_heatmaps = "symmetric"
-        self._pad_mode_segmentation_maps = "symmetric"
+        self._pad_mode_heatmaps = "reflect"
+        self._pad_mode_segmentation_maps = "reflect"
         self._pad_cval_heatmaps = 0.0
         self._pad_cval_segmentation_maps = 0
 
